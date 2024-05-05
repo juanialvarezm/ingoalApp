e084b461242a169dcf94d8c0d4fbc9cc25182256
Juani <juanialvarezzm@gmail.com>
2024-05-05 16:18:07 +0200

    added

--- changaApp/src/screens/Index.js
+++ changaApp/src/screens/Index.js
@@ -18,7 +18,7 @@ const Index = () => {
     <Tab.Navigator>
       <Tab.Screen name="Home" options={{headerTintColor: "#000",headerStyle: {backgroundColor: '#f0f0f0'},}} component={HomeScreen} />
       <Tab.Screen name="Grupos" options={{headerShown:false,headerTintColor: "#000",headerStyle: {backgroundColor: '#f0f0f0'},}} component={GrupoScreen} />
-      <Tab.Screen name="Perfil" options={{headerShown:false}} component={Login} />
+      <Tab.Screen name="Perfil" options={{headerShown:false}} component={IsAuthenticated} />
 
     </Tab.Navigator>
     </>
