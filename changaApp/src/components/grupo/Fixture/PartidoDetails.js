7fab5f8ae714a45b89130739b9287e3939639ece
Juani <juanialvarezzm@gmail.com>
2024-07-25 00:22:37 +0200

    a

--- changaApp/src/components/grupo/Fixture/PartidoDetails.js
+++ changaApp/src/components/grupo/Fixture/PartidoDetails.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import { useRoute } from '@react-navigation/native'
 
@@ -8,15 +8,58 @@ const PartidoDetails = () => {
 
 
     return (
-    <View>
-      <Text>PartidoDetails</Text>
-      <Text>itemId: {param.equipoLocal?.nombre}</Text>
-      <Text>itemId: {param.equipoVisitante?.nombre}</Text>
+    <View style={styles.partidoDetails}>
+        <View style={styles.equipoDetails}>
+          <View style={styles.equipoLocalBox}>
+          <Image
+            source={{uri: param?.equipoLocal?.logo}}
+            style={{width:90,height:90}}
+            />     
+            <Text>{param.equipoLocal?.nombre}</Text>
+            <Text style={{color:"#000",fontSize:38}}> {param.resultadoLocal}</Text>
 
+          </View>
+          <View style={styles.equipoVisitanteBox}>
+          <Image
+            source={{uri: param?.equipoVisitante?.logo}}
+            style={{width:90,height:90}}
+            />        
+            <Text> {param.equipoVisitante?.nombre}</Text>
+            <Text style={{color:"#000",fontSize:38}}> {param.resultadoVisitante}</Text>
+
+          </View>
+        </View>
+        <View style={{}}>
+
+        </View>
       </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  partidoDetails:{
+    flex:1,
+    backgroundColor:"#f6f6f6"
+  },
+  equipoDetails:{
+    flexDirection:"row",
+    justifyContent: 'center',
+    marginTop:50,
+    elevation:5,
+    backgroundColor:"#fff",
+    borderRadius:25,
+    padding:20,
+    margin:20
+  },
+  equipoLocalBox:{
+    paddingRight:70,
+    alignItems:"center",
+    alignContent:"center"
+  },
+  equipoVisitanteBox:{
+    alignItems:"center",
+    alignContent:"center"
+  }
+})
 
 export default PartidoDetails
